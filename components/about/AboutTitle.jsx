23b1c02851db9e0c7877191f5430b8c5750424a6
boomieindahouse <prawitsanat@hotmail.com>
2025-01-10 15:05:31 +0700

    edit about title

--- components/about/AboutTitle.jsx
+++ components/about/AboutTitle.jsx
@@ -14,7 +14,7 @@ export default function Abouttitle() {
         <div className="w-2/3">
           <h1 className="text-3xl md:text-6xl lg:text-6xl font-serif mb-14">
             <p className="text-sm text-gray-500 font-sans text-start mb-10">About us</p>
-            <span className="text-black leading-[1.3]">Join us on our journey and meet the amazing people behind our brand.</span>
+            <span className="text-black leading-[1.3]"><span className="text-teal-600">Join us on our journey </span>and meet the amazing people behind our brand.</span>
           </h1>
         </div>
       </div>
