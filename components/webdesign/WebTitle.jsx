1d76944cfec5a59371b576bc53997564883b58a3
boomieindahouse <prawitsanat@hotmail.com>
2025-01-11 02:26:10 +0700

    edit web design title res

--- components/webdesign/WebTitle.jsx
+++ components/webdesign/WebTitle.jsx
@@ -19,8 +19,8 @@ export default function WebTitle() {
             </div>
 
             <div className="container mx-auto px-4 relative z-10">
-                <div className="w-2/3">
-                    <h1 className="text-3xl md:text-6xl lg:text-5xl font-serif mb-14">
+                <div className="w-1/2">
+                    <h1 className="text-3xl md:text-4xl lg:text-5xl font-serif mb-14">
                         <p className="text-sm text-gray-500 font-sans text-start mb-10">Web Design</p>
                         <span className="text-black leading-[1.3]">Custom Website Development tailored to your needs.</span>
                     </h1>
