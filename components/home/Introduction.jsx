aae86af027c557719480da269a7b72d7424d04d6
boomieindahouse <prawitsanat@hotmail.com>
2025-01-12 16:10:19 +0700

    add experiences

--- components/home/Introduction.jsx
+++ components/home/Introduction.jsx
@@ -11,11 +11,29 @@ export default function Introduction() {
                 Meeting Creative
               </span>
               , we provides a comprehensive range of services designed to elevate your business in the digital world.{" "}
-              </p>
-              <p className='text-[12pt] lg:text-base text-gray-600'>
+            </p>
+            <p className='text-[12pt] lg:text-base text-gray-600'>
               Our team of experts is dedicated to providing top-notch solutions tailored to meet your unique needs.
-              </p>
-            
+            </p>
+
+            <div className="mt-10 grid grid-cols-3 sm:grid-cols-3 gap-8 text-start">
+              <div>
+                <h2 className="text-xl lg:text-4xl font-medium text-gray-800">7 Years</h2>
+                <p className="text-gray-600 text-sm lg:text-base mt-2">in business</p>
+              </div>
+              <div>
+                <h2 className="text-xl lg:text-4xl font-medium text-gray-800">120+</h2>
+                {/* สำหรับหน้าจอ lg และ md แสดง completed projects */}
+                <p className="hidden md:block text-gray-600 text-sm lg:text-base mt-2">completed projects</p>
+
+                {/* สำหรับหน้าจอ sm แสดงแค่ projects */}
+                <p className="block md:hidden text-gray-600 text-sm lg:text-base mt-2">projects</p>
+              </div>
+              <div>
+                <h2 className="text-xl lg:text-4xl font-medium text-gray-800">20+</h2>
+                <p className="text-gray-600 text-sm lg:text-base mt-2">clients</p>
+              </div>
+            </div>
           </div>
 
           <div className="relative">
